003f0c3792040a663f0a4919820d9bc7a26c0312
tvminh <van-minh.tran@spiritlabs.co>
2022-04-29 16:10:49 +0700

    [feat] filter attributes, move sellAddress and identifiers to OrdersFilterQuery

---
+++ core/ui/src/components/PoweredBy/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import IconCandyShop from 'assets/IconCandyShop';
+
+export const PoweredBy: React.FC = () => {
+  return (
+    <div className="candy-powered-by">
+      <a href="https://github.com/LIQNFT/candy-shop" target="_blank">
+        <IconCandyShop />
+      </a>
+    </div>
+  );
+};
